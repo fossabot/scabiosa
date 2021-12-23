8eb12fd51ba0c00d60ebb7709b5f971c7c327c21
netbenix <netbenix@codenoodles.de>
2021-12-23 09:22:13 +0100

    refactor(StorageInterface): Spacing fix

--- StorageTypes/StorageInterface.go
+++ StorageTypes/StorageInterface.go
@@ -12,15 +12,17 @@ func UploadFile(storage Storage, fileName string, backupName string, destination
 
 func CheckStorageType(storageType string) Storage{
 
-	if storageType == "azure-fileshare"{
+	if storageType == "azure-fileshare" {
 		return GetAzureStorage()
 	}
+
 	return nil
 }
 
 func CheckRemoteStorageType(storageType string) SQL.RemoteStorageType {
-	if storageType == "azure-fileshare"{
+	if storageType == "azure-fileshare" {
 		return SQL.REMOTE_AZURE_FILE
 	}
-	return 3
+
+	return SQL.REMOTE_NONE
 }
