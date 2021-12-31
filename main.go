d2f47ad6041c49aaa296c5d66c9eb5fb289f1882
netbenix <netbenix@codenoodles.de>
2021-12-31 15:34:14 +0100

    refactor(General): Moved process to command

--- main.go
+++ main.go
@@ -1,74 +1,33 @@
 package main
 
 import (
-	"github.com/google/uuid"
+	"github.com/urfave/cli/v2"
 	"os"
-	"scabiosa/Compressor"
+	"scabiosa/Commands"
 	"scabiosa/Logging"
-	"scabiosa/SQL"
-	"scabiosa/StorageTypes"
-	"scabiosa/Tools"
-	"time"
 )
 
-func main(){
-	Tools.CheckIfConfigExists()
-	config := Tools.GetConfig()
+func main() {
+	logger := Logging.Logger("mainThread")
 
-	SQL.CreateDefaultTables(SQL.GetSQLInstance())
-
-	for _, backupItem := range config.FolderToBackup{
-
-		var storage StorageTypes.Storage
-		var destPath string
-
-		if backupItem.RemoteStorageType != "none"{
-			storage = StorageTypes.CheckStorageType(backupItem.RemoteStorageType)
-			destPath = checkTmpPath(backupItem.CreateLocalBackup, backupItem.LocalTargetPath)
-		} else {
-			destPath = backupItem.LocalTargetPath
-		}
-
-		bakFile := Compressor.CreateBakFile(backupItem.BackupName + getTimeSuffix(), backupItem.FolderPath, destPath, backupItem.BackupName)
-
-		if backupItem.RemoteStorageType != "none"{
-			StorageTypes.UploadFile(storage, bakFile, backupItem.BackupName, backupItem.RemoteTargetPath)
-		}
-
-		if !backupItem.CreateLocalBackup && backupItem.RemoteStorageType != "none"{
-			backupItem.LocalTargetPath = "NONE"
-
-			_ = os.Remove(bakFile)
-			SQL.NewLogEntry(SQL.GetSQLInstance(), uuid.New(), SQL.LogInfo, backupItem.BackupName, SQL.SQLStage_DeleteTmp, SQL.REMOTE_NONE, "Deleted tmp file" ,time.Now())
-		}
-
-		if backupItem.RemoteStorageType == "none" {
-			backupItem.CreateLocalBackup = true
-			backupItem.RemoteTargetPath = "NONE"
-		}
-		SQL.NewBackupEntry(SQL.GetSQLInstance(), backupItem.BackupName, time.Now(), backupItem.CreateLocalBackup, backupItem.FolderPath, StorageTypes.CheckRemoteStorageType(backupItem.RemoteStorageType), backupItem.RemoteTargetPath, backupItem.LocalTargetPath)
+	app := &cli.App{
+		Name:  "scabiosa",
+		Usage: "Backup Util",
+		Authors: []*cli.Author{
+			{
+				Name:  "netbenix",
+				Email: "netbenix@codenoodles.de",
+			},
+		},
+		Copyright: "(c) 2021-2022 netbenix",
+		Commands: []*cli.Command{
+			Commands.NewStartBackupProc(),
+		},
 	}
 
-}
-
-
-func getTimeSuffix() string{
-	currTime := time.Now()
-
-	return "_" + currTime.Format("02-01-2006_15-04")
-}
-
-func checkTmpPath(createLocalBackup bool, targetPath string) string{
-	logger := Logging.DetailedLogger("mainThread", "checkTmpPath")
-	if !createLocalBackup {
-		if _, err := os.Stat("tmp"); os.IsNotExist(err) {
-			dirErr := os.Mkdir("tmp", 0775)
-			if dirErr != nil {
-				logger.Fatal(err)
-			}
-		}
-		return "tmp"
+	err := app.Run(os.Args)
+	if err != nil {
+		logger.Fatal(err)
 	}
 
-	return targetPath
 }
