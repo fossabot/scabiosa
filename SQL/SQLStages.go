922255624d85a2358e2ee67561aae56e47663a1f
netbenix <netbenix@codenoodles.de>
2022-01-31 20:10:22 +0100

    refactor(SQLStages): Added String() func

--- SQL/SQLStages.go
+++ SQL/SQLStages.go
@@ -1,9 +1,24 @@
 package SQL
 
+import "fmt"
+
 type SQLStage int64
 
-const(
-	SQLStage_Compress = 1
-	SQLStage_Upload = 2
+const (
+	SQLStage_Compress  = 1
+	SQLStage_Upload    = 2
 	SQLStage_DeleteTmp = 3
 )
+
+func (e SQLStage) String() string {
+	switch e {
+	case SQLStage_Compress:
+		return "COMPRESS"
+	case SQLStage_Upload:
+		return "UPLOAD"
+	case SQLStage_DeleteTmp:
+		return "DELETE TMP"
+	default:
+		return fmt.Sprintf("%d", e)
+	}
+}
