437512982bc05586e06a3b3f0869400379baee33
netbenix <netbenix@codenoodles.de>
2022-11-07 08:32:51 +0100

    refactor(Logging): Implemented new logging logic for ConfigHandling

--- Tools/Config.go
+++ Tools/Config.go
@@ -36,32 +36,32 @@ type Config struct {
 }
 
 func readConfig() []byte {
-	logger := Logging.BasicLog
+	logger := Logging.GetLoggingInstance()
 
 	file, err := os.ReadFile("config/config.json")
 	if err != nil {
-		logger.Fatal(err)
+		Logging.NewFatalEntry(logger, Logging.LogEntry{Message: err.Error(), CurrModule: "Config"})
 	}
 	return file
 }
 
 func readSQLConfig() []byte {
-	logger := Logging.BasicLog
+	logger := Logging.GetLoggingInstance()
 
 	file, err := os.ReadFile("config/sql-config.json")
 	if err != nil {
-		logger.Fatal(err)
+		Logging.NewFatalEntry(logger, Logging.LogEntry{Message: err.Error(), CurrModule: "Config"})
 	}
 	return file
 }
 
 func CheckIfConfigExists() {
-	logger := Logging.BasicLog
+	logger := Logging.GetLoggingInstance()
 
 	if _, err := os.Stat("config/config.json"); os.IsNotExist(err) {
 		_, fileErr := os.OpenFile("config/config.json", os.O_CREATE, 0600)
 		if fileErr != nil {
-			logger.Fatal(fileErr)
+			Logging.NewFatalEntry(logger, Logging.LogEntry{Message: fileErr.Error(), CurrModule: "Config"})
 		}
 		fmt.Printf("No configs detected. Please use 'scabiosa generate-config'\n")
 		os.Exit(0)
@@ -69,72 +69,69 @@ func CheckIfConfigExists() {
 }
 
 func GenerateBaseConfig() {
-	logger := Logging.BasicLog
+	logger := Logging.GetLoggingInstance()
 	var baseConfig Config
 
 	conf, err := json.MarshalIndent(baseConfig, "", "\t")
 	if err != nil {
-		logger.Fatal(err)
+		Logging.NewFatalEntry(logger, Logging.LogEntry{Message: err.Error(), CurrModule: "Config"})
 	}
 	for _, s := range baseConfig.FolderToBackup {
 		s.BackupName = ""
 	}
 	err = os.WriteFile("config/config.json", conf, 0600)
 	if err != nil {
-		logger.Fatal(err)
+		Logging.NewFatalEntry(logger, Logging.LogEntry{Message: err.Error(), CurrModule: "Config"})
 	}
-
 }
 
 func GenerateAzureConfig(azure AzureConfig) {
-	logger := Logging.BasicLog
+	logger := Logging.GetLoggingInstance()
 
 	conf, err := json.MarshalIndent(azure, "", "\t")
 	if err != nil {
-		logger.Fatal(err)
+		Logging.NewFatalEntry(logger, Logging.LogEntry{Message: err.Error(), CurrModule: "Config"})
 	}
 
 	err = os.WriteFile("config/azure.json", conf, 0600)
 	if err != nil {
-		logger.Fatal(err)
+		Logging.NewFatalEntry(logger, Logging.LogEntry{Message: err.Error(), CurrModule: "Config"})
 	}
 }
 
 func GenerateSQLConfig(sqlConfig *SQLConfig) {
-	logger := Logging.BasicLog
+	logger := Logging.GetLoggingInstance()
 
 	conf, err := json.MarshalIndent(sqlConfig, "", "\t")
 	if err != nil {
-		logger.Fatal(err)
+		Logging.NewFatalEntry(logger, Logging.LogEntry{Message: err.Error(), CurrModule: "Config"})
 	}
 
 	err = os.WriteFile("config/sql-config.json", conf, 0600)
 	if err != nil {
-		logger.Fatal(err)
+		Logging.NewFatalEntry(logger, Logging.LogEntry{Message: err.Error(), CurrModule: "Config"})
 	}
-
 }
 
 func GetSQLConfig() SQLConfig {
-	logger := Logging.BasicLog
+	logger := Logging.GetLoggingInstance()
 	var sqlConfig SQLConfig
 
 	err := json.Unmarshal(readSQLConfig(), &sqlConfig)
 	if err != nil {
-		logger.Fatal(err)
+		Logging.NewFatalEntry(logger, Logging.LogEntry{Message: err.Error(), CurrModule: "Config"})
 	}
 
 	return sqlConfig
 }
 
 func GetConfig() Config {
-
-	logger := Logging.BasicLog
+	logger := Logging.GetLoggingInstance()
 	var config Config
 
 	err := json.Unmarshal(readConfig(), &config)
 	if err != nil {
-		logger.Fatal(err)
+		Logging.NewFatalEntry(logger, Logging.LogEntry{Message: err.Error(), CurrModule: "Config"})
 	}
 
 	return config
