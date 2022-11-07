5894dbf055e2334604468d52b20eda25b7e30069
netbenix <netbenix@codenoodles.de>
2022-11-07 08:32:09 +0100

    refactor(Logging): Implemented new logging logic for LocalStorage

--- StorageTypes/LocalStorage.go
+++ StorageTypes/LocalStorage.go
@@ -1,7 +1,6 @@
 package StorageTypes
 
 import (
-	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -13,29 +12,29 @@ import (
 type LocalStorage struct{}
 
 func (LocalStorage) upload(fileName, backupName, destinationPath string) {
-	logger := Logging.BasicLog
+	logger := Logging.GetLoggingInstance()
 
 	srcFile, srcErr := os.Open(fileName)
 	if srcErr != nil {
-		logger.Fatal(srcErr)
+		Logging.NewFatalEntry(logger, Logging.LogEntry{Message: srcErr.Error(), CurrBackup: backupName, CurrDest: destinationPath, CurrModule: "LocalStorage"})
 	}
 	defer srcFile.Close()
 
 	destFile, destErr := os.OpenFile(destinationPath+string(os.PathSeparator)+filepath.Base(fileName), os.O_CREATE|os.O_RDWR, os.FileMode(0600))
 	if destErr != nil {
 		SQL.NewLogEntry(SQL.GetSQLInstance(), Logging.LogFatal, backupName, SQL.SqlStageUpload, SQL.RemoteNone, destinationPath, destErr.Error(), time.Now())
-		logger.Fatal(destErr)
+		Logging.NewFatalEntry(logger, Logging.LogEntry{Message: destErr.Error(), CurrBackup: backupName, CurrDest: destinationPath, CurrModule: "LocalStorager"})
 	}
 
 	defer destFile.Close()
 	SQL.NewLogEntry(SQL.GetSQLInstance(), Logging.LogInfo, backupName, SQL.SqlStageUpload, SQL.RemoteNone, destinationPath, "Starting copy process", time.Now())
-	logger.Info(fmt.Sprintf("[%s]Starting copy to %s", backupName, destinationPath))
+	Logging.NewInfoEntry(logger, Logging.LogEntry{Message: "Starting copy to destination.", CurrBackup: backupName, CurrDest: destinationPath, CurrModule: "LocalStorage"})
 	if _, err := io.Copy(destFile, srcFile); err != nil {
 		SQL.NewLogEntry(SQL.GetSQLInstance(), Logging.LogFatal, backupName, SQL.SqlStageUpload, SQL.RemoteNone, destinationPath, err.Error(), time.Now())
-		logger.Fatal(err)
+		Logging.NewFatalEntry(logger, Logging.LogEntry{Message: err.Error(), CurrBackup: backupName, CurrDest: destinationPath, CurrModule: "LocalStorage"})
 	}
 	SQL.NewLogEntry(SQL.GetSQLInstance(), Logging.LogInfo, backupName, SQL.SqlStageUpload, SQL.RemoteNone, destinationPath, "Finished copy process.", time.Now())
-	logger.Info(fmt.Sprintf("[%s]Copy finished.", backupName))
+	Logging.NewInfoEntry(logger, Logging.LogEntry{Message: "Copy finished.", CurrBackup: backupName, CurrDest: destinationPath, CurrModule: "LocalStorage"})
 }
 
 func GetLocalStorage() LocalStorage {
